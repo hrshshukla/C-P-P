f4a705e34a4bacde72a155dc87334021307d8415
Harsh Shukla <talktoharshshukla@gmail.com>
2024-09-29 11:10:32 -0700

    Question

---
+++ 03_Header files & Operators/3_headerFile_deep_dive.cpp
@@ -0,0 +1,3 @@
+// A deep conversation of Header file with Chatgpt (read whole chat)
+
+// https://chatgpt.com/share/66f91118-edcc-8013-9762-16c573ec3733
--- 15_Recursions & Recursive Functions/00_Recursions.cpp
+++ 15_Recursions & Recursive Functions/00_Recursions.cpp
@@ -5,7 +5,7 @@
 // 6! = 720 
 using namespace std;
 int factorial(int n){
-    if (n<=1){ // 1 is chota number nhi accept krega
+    if (n<=1){ // 1 is chota number accept krega toh 1 return kr dega 
         return 1;
     }
     
