abb4595a214587e9ded5b05763e88d1bc9789266
Harsh Shukla <talktoharshshukla@gmail.com>
2024-11-13 00:35:25 +0530

    Vector

--- 60_Vector_And_Advance/02_push_pop_back.cpp
+++ 60_Vector_And_Advance/02_push_pop_back.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <vector>
 // Hum seekhge <vector> me elemnts ko add krne aur delet krne ka method.
-// (1) push_back() -----> <vector> ke last me ek element create karega means agar tumne likha, push_back(4) toh (last_element = 4) hojayega.
-// (2) pop_back()  -----> <vector> ke last ek element ko delet kardega means agar tumne likha, pop_back() toh (last_element = deleted).
+// (1) push_back() -----> <vector> ke last me [ek element] create karega means agar tumne likha, push_back(4) toh (last_element = 4) hojayega.
+// (2) pop_back()  -----> <vector> ke last [ek element] ko delet kardega means agar tumne likha, pop_back() toh (last_element = deleted).
+
+// Benefit of Using pop_back() : we can add unlimited (elements) in a <vector> 
 
 // Learning :
 // ------> for (int num : number ); means (num) ke andar ek-ek krke (vector) ki har value dalo, means
 // ------> 1st iteration me : (num = 5)
 // ------> 2nd Iteration me : (num = 10)
 // ------> 3rd Iteration me : (num = 15)
-
 // Ye (for loop) kitne iteration run hoga ye (number of elemnts in vector) pe depend krta hai 
 using namespace std;
 
--- 61_String_Advance/02_substring_with_Loop.cpp
+++ 61_String_Advance/02_substring_with_Loop.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <unistd.h> // sleep(1) = 1 second
+#include <windows.h> // Sleep(1000) = 1 second 
+
 
 using namespace std;
 void checkAlphabet(string input_string){
@@ -10,7 +13,9 @@ void checkAlphabet(string input_string){
             
             for (char ch = 'A'; ch <= input_string[letter]; ch++) {
                 cout << input_string.substr(0, letter) << ch << endl;
+                Sleep(100); // delay (0.1) sec
             }
+            cout << endl;
         } 
 
         // Ye sirf small letters ke liye hai 
@@ -18,14 +23,16 @@ void checkAlphabet(string input_string){
             
             for (char ch = 'a'; ch <= input_string[letter]; ch++) {
                 cout << input_string.substr(0, letter) << ch << endl;
+                Sleep(100); // delay (0.1) sec
             }
+            cout << endl;
         }    
     }
  }
 
 int main(){
     string input;
-    cout << "Enter Name : "  << endl;
+    cout << "ENTER  : " ;
     cin>>input;
 
     checkAlphabet(input);
